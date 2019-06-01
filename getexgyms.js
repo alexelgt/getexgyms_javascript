ccd5e76f30bed8c82f6ebff3f049a67226a0da7a
Alexelgt <alexelgt@MacBook-Pro-de-Alejandro.local>
2019-06-01 18:24:41 +0200

    Tesr for iPhone X

--- getexgyms.js
+++ getexgyms.js
@@ -734,3 +734,22 @@ function isAutomaticModeRunning() {
         document.getElementById("btngetexandblockedgyms").disabled = true;
     }
 }
+
+function triggerSideBar() {
+    sidebarDiv = document.getElementById("sidebar");
+    overlay = document.getElementById("sidebar-overlay");
+
+    if (sidebarDiv.classList.contains("out")) {
+        sidebarDiv.classList.remove("out");
+        overlay.classList.remove("active");
+        overlay.classList.add("semiactive");
+        setTimeout(function(){
+            overlay.classList.remove("semiactive");
+        },200);
+    }
+    else {
+        sidebarDiv.classList.add("out");
+        overlay.classList.add("active");
+    }
+
+}
