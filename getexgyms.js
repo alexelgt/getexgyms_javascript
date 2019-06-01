71ea865b642d7ba39e0f8c849918c245ca24a966
Alexelgt <alexelgt@MacBook-Pro-de-Alejandro.local>
2019-06-01 22:04:53 +0200

    Reverted back the changes

--- getexgyms.js
+++ getexgyms.js
@@ -609,7 +609,6 @@ function resetareas() {
 function setMode(mode,pressed_div) {
 
     var parentClass = pressed_div.parentNode.className;
-    var pressedSelected = pressed_div.classList.contains("selected")
 
     if (parentClass != "") {
         parentClass = "." + parentClass.replace(/ /g, '.');
@@ -619,16 +618,12 @@ function setMode(mode,pressed_div) {
     var elems = document.querySelectorAll("#button_structure" + parentClass + " > div");
 
     [].forEach.call(elems, function(el) {
-        if (!pressedSelected) {
-            el.classList.remove("selected");
-        }
+        el.classList.remove("selected");
     });
     /*== Remove class "selected" from all elements ==*/
 
     /*=== Add class "selected" to element who triggered the function ===*/
-    setTimeout(function(){
-        pressed_div.classList.add("selected");
-    },250);
+    pressed_div.classList.add("selected");
 
     changeModeVar(mode);
 
