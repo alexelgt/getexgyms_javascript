65c4b9d585068fcc395b8bddae96d8e28bef641d
Alexelgt <alexelgt@MacBook-Pro-de-Alejandro.local>
2019-06-02 20:08:51 +0200

    Test with fixed position

--- sideBar.js
+++ sideBar.js
@@ -1,13 +1,16 @@
+var scrollTop_whenSidebarOpen;
+
 function triggerSideBar() {
-    sidebarDiv = document.getElementById("sidebar");
-    overlay = document.getElementById("sidebar-overlay");
+    var sidebarDiv = document.getElementById("sidebar");
+    var overlay = document.getElementById("sidebar-overlay");
 
     if (sidebarDiv.classList.contains("out")) {
         sidebarDiv.classList.remove("out");
         overlay.classList.remove("active");
         overlay.classList.add("semiactive");
-        document.getElementById("sidebar-overlay").removeEventListener("touchmove", freezeVp, false);
-        document.body.style.overflow = "visible";
+        document.body.style.position = "static";
+        document.getElementById("structure").style.top = 0;
+        document.body.scrollTop = scrollTop_whenSidebarOpen;
         setTimeout(function(){
             overlay.classList.remove("semiactive");
         },200);
@@ -15,12 +18,9 @@ function triggerSideBar() {
     else {
         sidebarDiv.classList.add("out");
         overlay.classList.add("active");
-        document.body.style.overflow = "hidden";
-        document.getElementById("sidebar-overlay").addEventListener("touchmove", freezeVp, false);
+        scrollTop_whenSidebarOpen = document.body.scrollTop;
+        document.body.style.position = "fixed";
+        document.getElementById("structure").style.top = -scrollTop_whenSidebarOpen;
     }
 
 }
-
-var freezeVp = function(e) {
-    document.getElementById("sidebar-overlay").preventDefault();
-};
